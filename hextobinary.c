c7df3a59650d0285451aa3edbecaa0ddd3831db1
Jax Tobyas <153058960+JaxT05@users.noreply.github.com>
2025-04-21 14:05:42 -0400

    initial commit

---
+++ hextobinary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    return 0;
+}
+
+// Hints: Try tackling the problem into parts.
+// 1.	Read a digit (character) from the user.
+// 2.	Translate it to the corresponding integer value. For example, 'A' is 10, 'E' is 14, '3' is 3, etc.
+// 3.	Use bit wise operations to print 0 or 1 for each bit in the integer value.
